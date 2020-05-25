c57864cebd14490ff2b9bc4fface612b4180ab53
ManaliA23 <64467845+ManaliA23@users.noreply.github.com>
2020-05-25 14:21:17 +0530

    Update cnnmodel.py

--- cnnmodel.py
+++ cnnmodel.py
@@ -1,39 +1,96 @@
-from keras.datsets import mnist
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-#reshape data to fit model
-X_train = X_train.reshape(60000,28,28,1)
-X_test = X_test.reshape(10000,28,28,1)
-
-from keras.utlis import to_categorical
-#one-hot encode target column
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
-
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from kera.layers import Dense, Conv2D, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.datasets import mnist
+from keras.utils import np_utils
+import keras
+
+
+
+
+
+# loads the MNIST dataset
+(x_train, y_train), (x_test, y_test)  = mnist.load_data()
+
+# Lets store the number of rows and columns
+img_rows = x_train[0].shape[0]
+img_cols = x_train[1].shape[0]
+
+# Getting our date in the right 'shape' needed for Keras
+# We need to add a 4th dimenion to our date thereby changing our
+# Our original image shape of (60000,28,28) to (60000,28,28,1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+# store the shape of a single image 
+input_shape = (img_rows, img_cols, 1)
+
+# change our image type to float32 data type
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalize our data by changing the range from (0 to 255) to (0 to 1)
+x_train /= 255
+x_test /= 255
+
+# Now we one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+num_pixels = x_train.shape[1] * x_train.shape[2]
+
+
+# create model
 model = Sequential()
-model.add(Conv2D(2, kernel_size=3, activation="relu", input_shape=(28,28,1)))
-i=1
-n=4
-for i in range(i):
-  model.add(Conv2D(filters=n,kernel_size=3,activation="relu"))
-  n=n*2
+
+
+
+
+
+
+
+
+model.add(Conv2D(50, (5, 5),
+                 padding = "same"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2)))
+
+# Fully connected layers (w/ RELU)
 model.add(Flatten())
-model.add(Dense(10, activation="softmax"))
+#model.add(Dense(500))
+#model.add(Activation("relu"))
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# Softmax (for classification)
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))
+           
+model.compile(loss = 'categorical_crossentropy',
+              optimizer = keras.optimizers.Adadelta(),
+              metrics = ['accuracy'])
+    
 
-model.fit(X_train, y_train, epochs=1)
 
-pred1=model.evaluate(X_test, y_test)
 
-print("Accuracy is : ", pred1[1]*100)
+# Training Parameters
+batch_size = 128
+epochs =  3
 
-try:
-  f=open("/newdir/o.txt","w")
-  f.write(str(int(pred1[1]*100)))
-except:
-  print(end"")
-finally:
-  f.close()
+history = model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True)
+
+
+
+# Evaluate the performance of our trained model
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+f= open("accuracy.txt","w+")
+f.write(str(scores[1]))
+f.close()
